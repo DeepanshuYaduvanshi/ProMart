35579e9744d3bb9913877331757456f16bfd7c69
DeepanshuYaduvanshi <deepanshu_12011111@nitkkr.ac.in>
2023-09-09 01:28:34 +0530

    change

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -32,16 +32,21 @@ import {
 } from "../constants/productConstants";
 
 // Get All Products
+// const instance = axios.create({
+//   baseURL : "https://pro-mart.vercel.app",
+// });
+const baseURL =  'https://pro-mart.vercel.app';
+
 export const getProduct =
   (keyword = "", currentPage = 1, price = [0, 25000], category, ratings = 0) =>
   async (dispatch) => {
     try {
       dispatch({ type: ALL_PRODUCT_REQUEST });
 
-      let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
+      let link = `${baseURL}/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
 
       if (category) {
-        link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
+        link = `${baseURL}/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
       }
 
       const { data } = await axios.get(link);
